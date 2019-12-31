7976f0173f2a6a58d117cc9dd2ed1fe92358c980
SUNGOLDSV <suraajvashisht@gmail.com>
2019-12-31 13:12:47 +0000

    redo last commit

--- init/init_violet.cpp
+++ init/init_violet.cpp
@@ -30,12 +30,17 @@
 #include <stdlib.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+
 #include <android-base/logging.h>
 #include <android-base/properties.h>
-#include "property_service.h"-
+#include "property_service.h"
+
 namespace android {
+
 namespace init {
-void property_override(char const prop[], char const value[]) {
+
+void property_override(char const prop[], char const value[])
+{
     prop_info *pi;
 
     pi = (prop_info*) __system_property_find(prop);
@@ -54,9 +59,9 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 
 void vendor_load_properties()
 {
-    // fingerprint
-    property_override("ro.build.description", "violet-user 9 PKQ1.181203.001 V10.3.9.0.PFHINXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.191205.011/5974828:user/release-keys");
+	property_override("ro.build.description", "violet-user 9 PKQ1.181203.001 V10.3.9.0.PFHINXM release-keys");
+	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.191205.011/5974828:user/release-keys");
 }
+
 } //init
 } //android
